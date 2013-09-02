de4e1fc9d4df60093d208b8882c4113070f5abcc
David Derby <dderby@server.fake>
2013-09-02 16:49:59 -0500

    Add ffmpeg code to unprocessed video model.

--- app/models/unprocessed_video.rb
+++ app/models/unprocessed_video.rb
@@ -1,3 +1,16 @@
+require "rubygems"
+require "ffmpeg"
+include FFMpeg
+
 class UnprocessedVideo < ActiveRecord::Base
 	attr_accessible :name, :message, :file
+	
+	def convertToMP4
+    convert self.file, :to => :mp4 do
+	    video_codec "libx264"
+	    audio_codec "aac"
+	    strictness "experimental"
+	    overwrite_existing_file
+    end.run
+	end
 end
